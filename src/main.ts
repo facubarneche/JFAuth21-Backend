4bcfefbb3109f7b40d70edc524cad2be4ae52e6c
Facundo Barneche <fh.barneche@gmail.com>
2025-02-11 21:06:07 -0300

    [FEATURE] Create files and connection with endpoint

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
+  console.info(`Running on port 3000`);
 }
 bootstrap();
